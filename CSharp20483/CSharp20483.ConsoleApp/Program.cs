0a84417b73279e21d870cb238d4a598c2f833797
Jhonegao <42944530+Jhonegao@users.noreply.github.com>
2020-02-08 14:52:19 -0300

    Inicio Remember

---
+++ CSharp20483/CSharp20483.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using CSharp20483.Tipos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp20483.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World\n");
+            TiposVariaveis tipos = new TiposVariaveis();
+            tipos.Executar();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharp20483/CSharp20483.Tipos/TiposVariaveis.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp20483.Tipos
+{
+    public class TiposVariaveis
+    {
+
+        public void Executar()
+        {
+            Console.WriteLine("Tipos: ");
+            Console.WriteLine("Tipos por valor");
+            int num1 = 10;
+            int num2 = num1;
+            //juntando os valores conventendo os inteiro para string;
+            Console.WriteLine("Valor inteiros: "+num1 + " e " + num2 );
+            num1 = 20;
+            //Metodo mais atual interpolação de strings, usase $ no começo, e as variaveis entre {}
+            Console.WriteLine($"Valores inteiros {num1} e {num2}");
+
+            //Modifica ambos pois estão aprontando pro mesmo ponto da memoria
+            Console.WriteLine("Tipos por referencia");
+            Pessoa p1 = new Pessoa { Idade = 10, Nome = "Joao" };
+            Pessoa p2 = p1;
+            Console.WriteLine($"Pessoas: {p1.Nome}-{p1.Idade} ---- {p2.Nome} - {p2.Idade}");
+            p1.Nome = "PEDRO";
+            Console.WriteLine($"Segunda impressão Pessoas: {p1.Nome}-{p1.Idade} ---- {p2.Nome} - {p2.Idade}");
+        }
+
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            public int Idade { get; set; }
+
+        }
+
+    }
+}

e0fc2b85eae0300d8c255f8e9132cf624239fe8e
Jhonegao <42944530+Jhonegao@users.noreply.github.com>
2020-02-08 17:49:50 -0300

    Casting

--- CSharp20483/CSharp20483.Tipos/UsandoTipos.cs
+++ CSharp20483/CSharp20483.Tipos/UsandoTipos.cs
@@ -20,9 +20,15 @@ namespace CSharp20483.Tipos
 
             long n3 = 10;
             short n4 = 20;
-            // nesse caso ele pega a maior atribuição e seta para ele mesmo.
+            //Nesse caso ele pega a maior atribuição e seta para ele mesmo.
             var result2 = n3 + n4;
             Console.WriteLine($"A soma é {result2} e resulta em um tipo {result2.GetType()}");
+
+            //Conversão explicita
+            var result3 = (int)n2;
+            //usando classe .net, mas tem o mesmo sentido.
+            var result4 = Convert.ToInt32(n2);
+            Console.WriteLine($"A soma é {result3} e resulta em um tipo {result3.GetType()}");
         }   
     }
 }

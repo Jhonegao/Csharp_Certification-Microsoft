90f0bb4fe7190e1dcff06980720e29878305ea91
Jhonegao <42944530+Jhonegao@users.noreply.github.com>
2020-02-08 15:42:55 -0300

    TipoValores

--- CSharp20483/CSharp20483.Tipos/TipoPorValor.cs
+++ CSharp20483/CSharp20483.Tipos/TipoPorValor.cs
@@ -13,7 +13,11 @@ namespace CSharp20483.Tipos
             Console.WriteLine("Tipos por valor");
             Console.WriteLine("Tipo Integrais");
             int num1 = 10;
-            Console.WriteLine($"Inteiros 32 {Int32.MinValue} - {Int32.MaxValue}");
+            byte numB = 4;
+            sbyte numsB = -10;
+            Console.WriteLine($"Byte {Byte.MinValue} - {Byte.MaxValue} ");
+            Console.WriteLine($"Inteiros 32 {Int32.MinValue} - {Int32.MaxValue} ");
+            Console.WriteLine($"SByte {SByte.MinValue} - {SByte.MaxValue} ");
         }
     }
 }

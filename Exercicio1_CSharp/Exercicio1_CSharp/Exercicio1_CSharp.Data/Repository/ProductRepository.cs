8ccc8dc2d411fd60257f6d533b6d1ff603b6bde8
Jhonegao <jhonatasimonetto@gmail.com>
2020-02-22 19:22:11 -0300

    Cruds

--- Exercicio1_CSharp/Exercicio1_CSharp/Exercicio1_CSharp.Data/Repository/ProductRepository.cs
+++ Exercicio1_CSharp/Exercicio1_CSharp/Exercicio1_CSharp.Data/Repository/ProductRepository.cs
@@ -10,30 +10,52 @@ namespace Exercicio1_CSharp.Data.Repository
 {
     public class ProductRepository
     {
-        
+
         public List<Product> ListAll()
         {
             List<Product> listProduct = new List<Product>();
-            Exercicio01_CSharpContex context = new Exercicio01_CSharpContex();
-            listProduct = context.Product.ToList();
-            context.Dispose();
+            //objeto usado neste contexto e sera descartado
+            using (Exercicio01_CSharpContex context = new Exercicio01_CSharpContex())
+            {
+                listProduct = context.Product.ToList();
+            }
             return listProduct;
         }
         public Product GetById(int id)
         {
             Product product = new Product();
+            using (Exercicio01_CSharpContex context = new Exercicio01_CSharpContex())
+            {
+                product = context.Product.Find(id);
+                //outro metodo, first ou Default pois nao se sabe quantos resultados vem
+                //product = context.Product.Where(p => p.Id == id).FirstOrDefault();
+            }
             return product;
         }
         public string Create(Product product)
         {
+            using (Exercicio01_CSharpContex context = new Exercicio01_CSharpContex())
+            {
+                context.Product.Add(product);
+                context.SaveChanges();
+            }
             return "Criado com sucesso!";
         }
         public string Update(Product product)
         {
+            using (Exercicio01_CSharpContex context = new Exercicio01_CSharpContex())
+            {
+                context.Entry<Product>(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
             return "Alterado com sucesso!!";
         }
         public string Delete(int id)
         {
+            using (Exercicio01_CSharpContex context = new Exercicio01_CSharpContex())
+            {
+                context.Product.Remove(GetById(id));
+            }
             return "Deletado com sucesso!!!";
         }
 
